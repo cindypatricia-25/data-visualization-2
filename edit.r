9487c8b649012388338795c7d0720dd82bbd6e81
cindypatricia <cpat0025@student.monash.edu>
2024-10-13 13:27:24 +1100

    gender age
    trying to make sunburst, but failed so use stacked bar instead for time. gender age using pyramid bar

--- edit.r
+++ edit.r
@@ -17,12 +17,36 @@ crash_summary <- data %>%
 # Group by 'State' and calculate the number of crashes per state, 
 # then select relevant columns
 selected_data <- data %>%
-  group_by(STATE_NAME, Day.of.week, Time.of.day, Dayweek) %>%
+  group_by(STATE_NAME, Time.of.day, Dayweek) %>%
   summarise(CrashCount = n()) %>%
   ungroup()
 
 # View the resulting data
-print(selected_data)
+#print(selected_data)
 
 # Optionally, save the selected and aggregated data to a new CSV file
-write.csv(selected_data, "sunburst.csv", row.names = FALSE)
+
+#write.csv(selected_data, "sunbursttt.csv", row.names = FALSE)
+
+# Load necessary libraries
+library(dplyr)
+
+# Create the dataset in the desired format by counting occurrences of each AgeGroup and Gender
+formatted_data <- data %>%
+  group_by(Age.Group, Gender) %>%
+  summarise(CrashCount = n(), .groups = 'drop')
+
+# View the transformed dataset
+#print(formatted_data)
+
+#write.csv(formatted_data, "genderage.csv", row.names = FALSE)
+
+crash_count_by_remoteness <- data %>%
+  group_by(National.Remoteness.Areas) %>%
+  summarise(CrashCount = n())
+
+# Print the result
+#print(crash_count_by_remoteness)
+
+# Optionally, write the result to a CSV file
+#write.csv(crash_count_by_remoteness, "crash_count_by_remoteness.csv", row.names = FALSE)
